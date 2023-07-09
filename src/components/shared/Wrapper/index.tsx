242733898a02b1d4b67eab2b2b044da28b229938
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-07-09 14:44:09 +0500

    Navbar logo updated

---
+++ src/components/shared/Wrapper/index.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react'
+
+const Wrapper: FC<{children: React.ReactNode}> = ({children}) => {
+  return (
+    <div className='max-w-7xl mx-auto px-2 md:px-6'>
+        {children}
+    </div>
+  )
+}
+
+export default Wrapper
--- src/components/view/Navbar/index.tsx
+++ src/components/view/Navbar/index.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="py-6  flex">
+      <div className="w-36">
+        <Image src={"/SiteLogo.png"} alt="Logo" width={500} height={500} />
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
