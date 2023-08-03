98eb0afb9deb2f81bcd49ef2fcea1dd5ed8dd2de
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-08-03 12:34:52 +0500

    navbar completed

--- src/components/view/Navbar/MobileView/MobileViewMenu.tsx
+++ src/components/view/Navbar/MobileView/MobileViewMenu.tsx
@@ -3,22 +3,30 @@ import {
   NavbarContext,
   useMobileView,
 } from "@/components/store/NavbarMenuContext";
+import {
+  NavbarItemType,
+  navbarItems,
+} from "@/components/utils/NavbarDataAndTypes";
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
 
-const MobileViewMenu = (props: Props) => {
+const MobileViewMenu = (_props: Props) => {
   const { isOpenMobileView, handleIsOpenMobileView } =
     useMobileView() as NavbarContext;
 
   return (
-    <div className="h-screen text-center mx-auto">
+    <div>
       {isOpenMobileView && (
-        <div>
-          <div>hellow</div>
-          <div>hellow</div>
-          <div>hellow</div>
-          <div>hellow</div>
+        <div className="mt-8 h-screen text-center mx-auto flex flex-col space-y-8">
+          {navbarItems.map((_navbarItem) => {
+            return (
+              <div>
+                <Link href={_navbarItem.href}>{_navbarItem.label}</Link>
+              </div>
+            );
+          })}
         </div>
       )}
     </div>
