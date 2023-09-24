3351fb9a4cf5d2d033ed988547e72f3c0c4df723
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-09-24 20:35:56 +0500

    logo section of footer completed

---
+++ src/app/components/view/Footer/Logo/index.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import logo from "../../../../../../public/Logo.webp";
+import { RiFacebookFill, RiLinkedinFill, RiTwitterXLine } from "react-icons/ri";
+
+type Props = {};
+
+const Logo = (props: Props) => {
+  return (
+    <div className="flex flex-col gap-8 w-full">
+      {/* Image */}
+      <Image src={logo} alt="Site Logo" className="w-[180px] h-[30px]" />
+
+      <p className="w-3/5 text-zinc-600 font-medium leading-tight">
+        Small, artisan label that offers a thoughtfully curated collection of
+        high quality everyday essentials made.
+      </p>
+
+      {/* Social Icons container */}
+      <div className="flex gap-4">
+        <div className="bg-zinc-200 py-[10px] px-[12px] rounded-lg shadow-lg cursor-pointer">
+          <RiTwitterXLine size={22} />
+        </div>
+        <div className="bg-zinc-200 py-[10px] px-[12px] rounded-lg shadow-lg cursor-pointer">
+          <RiFacebookFill size={22} />
+        </div>
+        <div className="bg-zinc-200 py-[10px] px-[12px] rounded-lg shadow-lg cursor-pointer">
+          <RiLinkedinFill size={22} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Logo;
--- src/app/components/view/Footer/index.tsx
+++ src/app/components/view/Footer/index.tsx
@@ -1,9 +1,28 @@
 import React from "react";
+import Logo from "./Logo";
 
 type Props = {};
 
 const Footer = (props: Props) => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      {/* Footer */}
+      <div>
+        <Logo />
+        {/* Company Links */}
+        <div></div>
+
+        {/* Support Links */}
+        <div></div>
+
+        {/* Contact Links */}
+        <div></div>
+      </div>
+
+      {/* Copyright */}
+      <div></div>
+    </footer>
+  );
 };
 
 export default Footer;
